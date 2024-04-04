43f5b148dde2ece26b878c5fdfbdce1a1104817b
lnormandon <lnormandon@openstudio.fr>
2024-04-04 14:01:57 +0200

    Add Traductions

--- I18n/fr_FR.php
+++ I18n/fr_FR.php
@@ -17,4 +17,7 @@ return array(
     'The sum of quantities of item in your cart must be lesser than 1000.' => 'La somme des quantités de produit dans votre panier doit être en dessous de 1000.',
     'You must either select a content or say you have added the hook for Oney terms and conditions' => 'Vous devez soit séléctionner un contenu soit valider que vous avez ajouté le Hook pour les CGV Oney',
     'Your email addresses cannot contain a plus sign (+).' => 'Votre adresse email ne doit pas contenir le signe plus (+).',
+    'Financing offer with compulsory contribution, reserved for private individuals and valid for any purchase from € 100.00 to € 3,000.00. Subject to acceptance by Oney Bank. You have 14 days to cancel your credit. Oney Bank - SA au capital de 51 286 585€ - 34 Avenue de Flandre 59170 Croix - 546 380 197 RCS Lille Métropole - n° Orias 07 023 261 www.orias.fr Correspondence: CS 60 006 - 59895 Lille Cedex - www.oney.fr' => 'Offre de financement avec apport obligatoire, réservée aux particuliers et valable pour tout achat de 100,00 € à 3 000,00 €. Sous réserve d\'acceptation par Oney Bank. Vous disposez d\'un délai de 14 jours pour renoncer à votre crédit. Oney Bank - SA au capital de 51 286 585€ - 34 Avenue de Flandre 59170 Croix - 546 380 197 RCS Lille Métropole - n° Orias 07 023 261 www.orias.fr Correspondance : CS 60 006 - 59895 Lille Cedex - www.oney.fr',
+    'Choose the number of payments' => 'Choisir le nombre de paiements',
+    'Pay in %count times' => 'Payer en %count fois'
 );
